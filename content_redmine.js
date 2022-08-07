5fd959a6d741cbd99b722142033bab46024799b6
Serhii Syrotenko <ssyrotenko@amcbridge.com>
2022-08-07 14:06:23 +0300

    Fix code formatting

--- content_redmine.js
+++ content_redmine.js
@@ -1,28 +1,30 @@
 function processGetIssueInfo() {
-  try {
-    var typeAndId = document.querySelector('#content > h2').textContent.split('#');
-    var name = document.querySelector('div.subject > div > h3').textContent;
-    return Promise.resolve({format: 'itn',
-                            type: typeAndId[0].trim(),
-                            id: `${typeAndId[1].trim()}`,
-                            name: name});
-  } catch (error) {
-    console.error(error);
-    return Promise.reject(error);
-  }
+    try {
+        var typeAndId = document.querySelector('#content > h2').textContent.split('#');
+        var name = document.querySelector('div.subject > div > h3').textContent;
+        return Promise.resolve({
+            format: 'itn',
+            type: typeAndId[0].trim(),
+            id: `${typeAndId[1].trim()}`,
+            name: name
+        });
+    } catch (error) {
+        console.error(error);
+        return Promise.reject(error);
+    }
 }
 
 function processRequest(request) {
-  switch (request.type) {
-    case 'get_issue_info':
-      return processGetIssueInfo();
-    default:
-      var message = `Unknown request ${request.type}`
-      console.log(message);
-      return Promise.reject(message);
-  }
+    switch (request.type) {
+        case 'get_issue_info':
+            return processGetIssueInfo();
+        default:
+            var message = `Unknown request ${request.type}`
+            console.log(message);
+            return Promise.reject(message);
+    }
 }
 
 browser.runtime.onMessage.addListener((request) => {
-  return processRequest(request);
+    return processRequest(request);
 });
